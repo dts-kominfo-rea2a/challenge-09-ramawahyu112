df68c7e56b6b5cf234d603383e0d93b689c0b69b
ramawahyu112 <52642373+ramawahyu112@users.noreply.github.com>
2022-06-26 21:39:37 +0800

    init react

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,10 +1,17 @@
 // Terima props lalu tampilkan dalam Contact component
 // Kalian bisa membuat CSS sendiri di src/components/Contact.css
 import React from 'react';
-
-const Contact = () => {
+import "./Contact.css";
+const Contact = (value) => {
     return (
-        <></>
+        <div className="contact-card">
+            <img className='contact-img' src={value.data.photo} alt={value.data.name} />
+            <div className='container'>
+                <h1>{value.data.name}</h1>
+                <h4>{value.data.phone}</h4>
+                <h4>{value.data.email}</h4>
+            </div>
+    </div>
     )
 }
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,14 @@
 // Kalian bisa menambahkan CSS di src/components/Header.css
 import React from 'react';
+import "./Header.css";
 
 const Header = () => {
     return (
-        <></>
+        <div class="header">
+            <h1>Call a Friend</h1>
+            <h5>Your friendly contact app</h5>
+            <span class='line-dot'>..............................</span>
+        </div>
     )
 }
 
